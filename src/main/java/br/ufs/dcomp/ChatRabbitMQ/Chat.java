3c8456f74888ef608c9c740f7517dedc46325d82
MarceloNascc <marcelo.nasccc@gmail.com>
2023-02-11 10:28:04 -0300

    feat: create base of the chat

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -1,34 +1,61 @@
 package br.ufs.dcomp.ChatRabbitMQ;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import com.rabbitmq.client.*;
 
-import java.io.IOException;
-
 public class Chat {
+  public static void main(String[] argv) throws IOException, TimeoutException {
+    String currentChatWith = "";
+    System.out.print("User: ");
 
-  public static void main(String[] argv) throws Exception {
+    Scanner input = new Scanner(System.in);
+    String userName = input.nextLine();
+
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("ip-da-instancia-da-aws"); // Alterar
-    factory.setUsername("usuário-do-rabbitmq-server"); // Alterar
-    factory.setPassword("senha-do-rabbitmq-server"); // Alterar
+    factory.setHost("54.237.84.182");
+    factory.setUsername("admin");
+    factory.setPassword("rabbitmq2022.2");
     factory.setVirtualHost("/");
     Connection connection = factory.newConnection();
-    Channel channel = connection.createChannel();
-    
-    String QUEUE_NAME = "minha-fila";
-                      //(queue-name, durable, exclusive, auto-delete, params); 
-    channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
-    
-    Consumer consumer = new DefaultConsumer(channel) {
-      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)           throws IOException {
+    Channel senderChannel = connection.createChannel();
+    Channel consumerChannel = connection.createChannel();
 
-        String message = new String(body, "UTF-8");
-        System.out.println(message);
-
+    Consumer consumer = new DefaultConsumer(consumerChannel) {
+      public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+        String[] message = new String(body, "UTF-8").split(":::");
+        LocalDateTime sentAt = LocalDateTime.parse(message[0]);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy às HH:mm:ss");
+        String messageSentAt = sentAt.format(formatter);
+        System.out.print("\033[1F\33[K");
+        System.out.println("\n(" + messageSentAt + ") " + message[1] + " diz: " + message[2]);
       }
     };
-                      //(queue-name, autoAck, consumer);    
-    channel.basicConsume(QUEUE_NAME, true,    consumer);
-    
+    senderChannel.queueDeclare(userName, false, false, false, null);
+    consumerChannel.basicConsume(userName, true, consumer);
+
+    System.out.print(">> ");
+    String userInput = input.nextLine();
+
+    while (!userInput.contentEquals("exit")) {
+      if (userInput.startsWith("@")) {
+        currentChatWith = userInput;
+        senderChannel.queueDeclare(currentChatWith.substring(1), false, false, false, null);
+      } else if (currentChatWith.length() > 0) {
+        LocalDateTime dateNow = LocalDateTime.now();
+        String message = dateNow + ":::" + currentChatWith.substring(1) + ":::" + userInput;
+        senderChannel.basicPublish("", currentChatWith.substring(1), null, message.getBytes("UTF-8"));
+      } else {
+        System.out.println("You must select who you want to chat with...");
+      }
+
+      System.out.print(currentChatWith + ">> ");
+      userInput = input.nextLine();
+    }
+
+    input.close();
   }
 }
