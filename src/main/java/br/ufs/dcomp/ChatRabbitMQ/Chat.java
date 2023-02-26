4da70b68ecb819c8064c304e3faac7fe3b55987a
ahamilton <antoniohamilton.s.freitas@gmail.com>
2023-02-26 00:30:26 +0000

    Etapa 2: adding proto structure + serializing and deserializing data

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -2,11 +2,55 @@ package br.ufs.dcomp.ChatRabbitMQ;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.time.ZoneId;
 import java.util.concurrent.TimeoutException;
 import java.util.Locale;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import com.rabbitmq.client.*;
+import com.google.protobuf.*;
+
+class Message {
+  ChatProto.Mensagem.Builder senderMessage = ChatProto.Mensagem.newBuilder();
+  ChatProto.Conteudo.Builder content = ChatProto.Conteudo.newBuilder();
+  
+  //Building TimeZone Date
+  Locale local = new Locale("pt", "BR");
+  DateTimeFormatter dateformat = DateTimeFormatter.ofPattern("dd-MM-yyyy", local);
+  DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm", local);
+  
+  // Setting senderMessage properties
+
+  public void setMessage (String userName, String type, String userInput) throws IOException, TimeoutException {
+    try {
+      // Getting and formating date-time
+      LocalDateTime dateNow = LocalDateTime.now(ZoneId.of("America/Sao_Paulo"));
+      String messageDate = dateNow.format(dateformat);
+      String messageTime = dateNow.format(timeFormat);
+      
+      byte[] userMessage = userInput.getBytes("UTF-8");
+      
+      // setting Content props
+      content.setTipo("text/plain");
+      content.setCorpo(ByteString.copyFrom(userMessage));
+      
+      // setting Message props
+      senderMessage.setEmissor(userName); 
+      senderMessage.setData(messageDate);
+      senderMessage.setHora(messageTime);
+      senderMessage.setConteudo(content);
+      
+    } catch (Exception err) {
+      
+      System.out.println(err);
+      
+    }
+  }
+  
+  public ChatProto.Mensagem getMessage () {
+    return this.senderMessage.build();
+  }
+}
 
 class ChatWith {
   private String user = "";
@@ -23,6 +67,7 @@ class ChatWith {
 public class Chat {
   public static void main(String[] argv) throws IOException, TimeoutException {
     try {
+
       
       ChatWith currentChatWith = new ChatWith();
       System.out.print("User: ");
@@ -31,7 +76,7 @@ public class Chat {
       String userName = input.nextLine();
   
       ConnectionFactory factory = new ConnectionFactory();
-      factory.setHost("107.21.41.1");
+      factory.setHost("52.87.222.213");
       factory.setUsername("admin");
       factory.setPassword("admin");
       factory.setVirtualHost("/");
@@ -41,24 +86,28 @@ public class Chat {
   
       Consumer consumer = new DefaultConsumer(consumerChannel) {
         public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-          String[] message = new String(body, "UTF-8").split(":::");
-          Locale local = new Locale("pt", "BR");
-          LocalDateTime sentAt = LocalDateTime.parse(message[0]);
-          DateTimeFormatter dateformat = DateTimeFormatter.ofPattern("dd-MM-yyyy", local);
-          DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm", local);
-          String messageDate = sentAt.format(dateformat);
-          String messageTime = sentAt.format(timeFormat);
-          System.out.println("\n(" + messageDate + " às " + messageTime + ") " + message[1] + " diz: " + message[2]);
+          ChatProto.Mensagem message = ChatProto.Mensagem.parseFrom(body);
+          ChatProto.Conteudo content = message.getConteudo();
+  
+          String sender = message.getEmissor();
+          String date = message.getData();
+          String time = message.getHora();
+          String bodyMessage = content.getCorpo().toStringUtf8();
+
+          System.out.println("\n(" + date + " às " + time + ") " + sender + " diz: " + bodyMessage);
+          
           if (currentChatWith.get().length() > 0) {
+            
             System.out.print(currentChatWith.get() + ">> ");
+            
           } else {
+            
             System.out.print(">> ");
+            
           }
   
         }
       };
-      
-  
       
       senderChannel.queueDeclare(userName, false, false, false, null);
       consumerChannel.basicConsume(userName, true, consumer);
@@ -67,13 +116,20 @@ public class Chat {
       String userInput = input.nextLine();
   
       while (!userInput.contentEquals("exit")) {
+
         if (userInput.startsWith("@")) {
+          
           currentChatWith.set(userInput);
           senderChannel.queueDeclare(currentChatWith.get().substring(1), false, false, false, null);
+          
         } else if (currentChatWith.get().length() > 0) {
-          LocalDateTime dateNow = LocalDateTime.now();
-          String message = dateNow + ":::" + userName.substring(0) + ":::" + userInput;
-          senderChannel.basicPublish("", currentChatWith.get().substring(1), null, message.getBytes("UTF-8"));
+          Message senderMessage = new Message();
+          senderMessage.setMessage(userName.substring(0), "text/plain", userInput);
+
+          ChatProto.Mensagem message = senderMessage.getMessage();
+          byte[] buffer = message.toByteArray();
+          
+          senderChannel.basicPublish("", currentChatWith.get().substring(1), null, buffer);
         } else {
           System.out.println("You must select who you want to chat with...");
         }
