60df4d5a292aa64f41da5e4101a32d2771c8b768
MarceloNascc <marcelo.nasccc@gmail.com>
2023-04-26 10:53:30 -0300

    feat: add listUsers and listGroups commands

--- src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
+++ src/main/java/br/ufs/dcomp/ChatRabbitMQ/Chat.java
@@ -15,6 +15,12 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import com.rabbitmq.client.*;
 import com.google.protobuf.*;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 class Message {
   ChatProto.Message.Builder senderMessage = ChatProto.Message.newBuilder();
@@ -27,7 +33,8 @@ class Message {
 
   // Setting senderMessage properties
 
-  public void setMessage(String userName, String groupName, String type, String userInput, Boolean isGroup, Boolean isFile) throws IOException, TimeoutException {
+  public void setMessage(String userName, String groupName, String type, String userInput, Boolean isGroup,
+      Boolean isFile) throws IOException, TimeoutException {
     try {
       // Getting and formating date-time
       LocalDateTime dateNow = LocalDateTime.now(ZoneId.of("America/Sao_Paulo"));
@@ -35,16 +42,16 @@ class Message {
       String messageTime = dateNow.format(timeFormat);
 
       content.setType(type);
-          
+
       if (isFile) {
         Path path = Paths.get(userInput);
         byte[] fileConvertedToByte = Files.readAllBytes(path);
         String fileNameSplitted[] = userInput.split("/");
-        content.setName(fileNameSplitted[fileNameSplitted.length-1]);
+        content.setName(fileNameSplitted[fileNameSplitted.length - 1]);
         content.setBody(ByteString.copyFrom(fileConvertedToByte));
       } else {
-          byte[] userMessage = userInput.getBytes("UTF-8");
-          content.setBody(ByteString.copyFrom(userMessage));
+        byte[] userMessage = userInput.getBytes("UTF-8");
+        content.setBody(ByteString.copyFrom(userMessage));
       }
 
       // setting Message props
@@ -77,14 +84,15 @@ class ChatWith {
 }
 
 class MessageRabbitMQ {
-  public void sendMessage (String userName, String userInput, ChatWith currentChatWith, Channel senderChannel, Boolean isFile) throws IOException, TimeoutException {
+  public void sendMessage(String userName, String userInput, ChatWith currentChatWith, Channel senderChannel,
+      Boolean isFile) throws IOException, TimeoutException {
     try {
       if (currentChatWith.get().length() > 0) {
         Message senderMessage = new Message();
-    
+
         boolean isChattingWithAGroup = currentChatWith.get().startsWith("#");
         String group = isChattingWithAGroup ? currentChatWith.get() : "";
-    
+
         if (!isFile) {
           senderMessage.setMessage(userName, group, "message", userInput, isChattingWithAGroup, isFile);
         } else {
@@ -92,25 +100,25 @@ class MessageRabbitMQ {
           String mimeType = Files.probeContentType(source);
           if (mimeType != null) {
             if (Files.exists(source)) {
-                senderMessage.setMessage(userName, group, mimeType, userInput, isChattingWithAGroup, isFile);
+              senderMessage.setMessage(userName, group, mimeType, userInput, isChattingWithAGroup, isFile);
             } else {
               System.out.println("This file doesn't exist, send a valid file path");
               System.out.print(currentChatWith.get() + ">> ");
-              return; 
+              return;
             }
           } else {
             System.out.println("Send a valid file");
             System.out.print(currentChatWith.get() + ">> ");
-            return; 
+            return;
           }
         }
-    
+
         ChatProto.Message message = senderMessage.getMessage();
         byte[] buffer = message.toByteArray();
-    
+
         String exchange = isChattingWithAGroup ? currentChatWith.get().substring(1) : "";
         String routingKey = !isChattingWithAGroup ? currentChatWith.get().substring(1) : "";
-    
+
         senderChannel.basicPublish(exchange, routingKey, null, buffer);
       } else {
         System.out.println("You must select who you want to chat with...");
@@ -119,8 +127,8 @@ class MessageRabbitMQ {
       System.out.println(e);
     }
   }
-  
-  public void receiveMessage (byte[] body, ChatWith currentChatWith) throws IOException {
+
+  public void receiveMessage(byte[] body, ChatWith currentChatWith) throws IOException {
     ChatProto.Message message = ChatProto.Message.parseFrom(body);
     ChatProto.Content content = message.getContent();
 
@@ -129,7 +137,7 @@ class MessageRabbitMQ {
     String time = message.getTime();
     String group = message.getGroup();
     String contentType = content.getType();
-    
+
     switch (contentType) {
       case "message":
         String bodyMessage = content.getBody().toStringUtf8();
@@ -138,13 +146,14 @@ class MessageRabbitMQ {
         break;
       default:
         String contentName = content.getName();
-        ReceiveFile receiveFileThread = new ReceiveFile(content.getBody().toByteArray(), contentName, currentChatWith, sender, group, date, time);
+        ReceiveFile receiveFileThread = new ReceiveFile(content.getBody().toByteArray(), contentName, currentChatWith,
+            sender, group, date, time);
         new Thread(receiveFileThread).start();
         break;
     }
   }
 }
-  
+
 class ReceiveFile implements Runnable {
   private byte[] fileBody;
   private ChatWith currentChatWith;
@@ -153,8 +162,9 @@ class ReceiveFile implements Runnable {
   private String group;
   private String date;
   private String time;
-  
-  public ReceiveFile (byte[] fileBody, String fileName, ChatWith currentChatWith, String sender, String group, String date, String time) {
+
+  public ReceiveFile(byte[] fileBody, String fileName, ChatWith currentChatWith, String sender, String group,
+      String date, String time) {
     this.fileBody = fileBody;
     this.currentChatWith = currentChatWith;
     this.fileName = fileName;
@@ -163,36 +173,37 @@ class ReceiveFile implements Runnable {
     this.date = date;
     this.time = time;
   }
-  
+
   @Override
-  public void run () {
+  public void run() {
     try {
       FileOutputStream fos = new FileOutputStream("download/" + fileName);
       fos.write(fileBody);
       fos.close();
-      System.out.println("\n(" + date + " at " + time + ") file " + "\"" + fileName + "\"" + " received from " + sender + group + "!");
+      System.out.println(
+          "\n(" + date + " at " + time + ") file " + "\"" + fileName + "\"" + " received from " + sender + group + "!");
       System.out.print(currentChatWith.get() + ">> ");
-    } catch (Exception e) { 
+    } catch (Exception e) {
       System.out.println("Something got wrong!");
       System.out.println(e);
     }
   }
 }
-  
+
 class SendFile implements Runnable {
-  private MessageRabbitMQ messageRabbitMQ = new MessageRabbitMQ(); 
+  private MessageRabbitMQ messageRabbitMQ = new MessageRabbitMQ();
   private String path;
   private String sender;
   private Channel senderChannel;
   private ChatWith currentChatWith;
-  
-  public SendFile(String sender, String path, ChatWith currentChatWith, Channel senderChannel){
+
+  public SendFile(String sender, String path, ChatWith currentChatWith, Channel senderChannel) {
     this.sender = sender;
     this.path = path;
     this.currentChatWith = currentChatWith;
     this.senderChannel = senderChannel;
   }
-  
+
   @Override
   public void run() {
     try {
@@ -208,11 +219,19 @@ class Commands {
   private Channel commandsChannel;
   private ChatWith currentChatWith;
   private String userName;
+  private WebTarget webTarget;
+  private String authorizationHeaderName;
+  private String authorizationHeaderValue;
 
-  public Commands(Connection conn, String currentUser, ChatWith currentChatWith) throws IOException {
+  public Commands(Connection conn, String currentUser, ChatWith currentChatWith, WebTarget target, String basicAuth)
+      throws IOException {
     this.commandsChannel = conn.createChannel();
     this.userName = currentUser;
     this.currentChatWith = currentChatWith;
+    this.webTarget = target;
+    this.authorizationHeaderName = "Authorization";
+    this.authorizationHeaderValue = "Basic "
+        + java.util.Base64.getEncoder().encodeToString(basicAuth.getBytes());
   }
 
   private void printCommands() {
@@ -222,6 +241,8 @@ class Commands {
     System.out.println("delFromGroup: remove a user from a group\nEx: !delFromGroup userName groupName\n");
     System.out.println("removeGroup: delete a group\nEx: !removeGroup groupName\n");
     System.out.println("upload: uploads a file to a group or person while is talking with it\nEx: !upload path\n");
+    System.out.println("listUsers: list users from a group\nEx: !listUsers groupName\n");
+    System.out.println("listGroups: list all groups from a user\nEx: !listGroups userName\n");
   }
 
   private boolean hasCorrectAmountOfArgs(String[] args, int length) {
@@ -251,10 +272,62 @@ class Commands {
   private void deleteGroup(String groupName) throws IOException {
     this.commandsChannel.exchangeDelete(groupName);
   }
-  
+
   private void uploadFile(String path) throws IOException {
     SendFile fileThread = new SendFile(userName, path, currentChatWith, commandsChannel);
     new Thread(fileThread).start();
+  }
+
+  private void listUsers(String group) {
+    Response response = this.webTarget
+        .path("/exchanges/%2f/" + group + "/bindings/source")
+        .request(MediaType.APPLICATION_JSON)
+        .header(authorizationHeaderName, authorizationHeaderValue)
+        .get();
+
+    String users = "";
+    if (response.getStatus() == 200) {
+      String json = response.readEntity(String.class);
+      JSONArray array = new JSONArray(json);
+
+      for (int i = 0; i < array.length(); i++) {
+        JSONObject obj = new JSONObject(array.get(i).toString());
+        String value = obj.get("destination").toString();
+        if (!value.isBlank()) {
+          users += ", " + value;
+        }
+      }
+
+      System.out.println(users.substring(2));
+    } else {
+      System.out.println("There's an error with the command listUsers: " + response.getStatus());
+    }
+  }
+
+  private void listGroups() {
+    Response response = this.webTarget
+        .path("/queues/%2f/" + this.userName + "/bindings")
+        .request(MediaType.APPLICATION_JSON)
+        .header(authorizationHeaderName, authorizationHeaderValue)
+        .get();
+
+    String groups = "";
+    if (response.getStatus() == 200) {
+      String json = response.readEntity(String.class);
+      JSONArray array = new JSONArray(json);
+
+      for (int i = 0; i < array.length(); i++) {
+        JSONObject obj = new JSONObject(array.get(i).toString());
+        String value = obj.get("source").toString();
+        if (!value.isBlank()) {
+          groups += ", " + value;
+        }
+      }
+
+      System.out.println(groups.substring(2));
+    } else {
+      System.out.println("There's an error with the command listGroups: " + response.getStatus());
+    }
   }
 
   public void executeCommand(String userInput) throws IOException {
@@ -284,11 +357,21 @@ class Commands {
         break;
       case "!upload":
         if (currentChatWith.get().isEmpty()) {
-         System.out.println("You must select who you want to send the file...");
-         break;
+          System.out.println("You must select who you want to send the file...");
+          break;
         }
         if (this.hasCorrectAmountOfArgs(args, 2)) {
           this.uploadFile(args[1]);
+        }
+        break;
+      case "!listUsers":
+        if (this.hasCorrectAmountOfArgs(args, 2)) {
+          this.listUsers(args[1]);
+        }
+        break;
+      case "!listGroups":
+        if (this.hasCorrectAmountOfArgs(args, 1)) {
+          this.listGroups();
         }
         break;
       default:
@@ -303,7 +386,7 @@ public class Chat {
   public static void main(String[] argv) {
     try {
       MessageRabbitMQ messageRabbitMQ = new MessageRabbitMQ();
-      
+
       ChatWith currentChatWith = new ChatWith();
       System.out.print("User: ");
 
@@ -311,16 +394,17 @@ public class Chat {
       String userName = input.nextLine();
 
       ConnectionFactory factory = new ConnectionFactory();
-      factory.setHost("rabbit-loadbalance-b6f5d4151e139b83.elb.us-east-1.amazonaws.com");
+      factory.setHost("rabbitmq-sd-20222-lb-0c36c1cf9b9311ed.elb.us-east-1.amazonaws.com");
       factory.setUsername("admin");
-      factory.setPassword("admin");
+      factory.setPassword("rabbitmq2022.2");
       factory.setVirtualHost("/");
       Connection connection = factory.newConnection();
       Channel senderChannel = connection.createChannel();
       Channel consumerChannel = connection.createChannel();
 
       Consumer consumer = new DefaultConsumer(consumerChannel) {
-        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+            throws IOException {
           messageRabbitMQ.receiveMessage(body, currentChatWith);
         }
       };
@@ -328,7 +412,15 @@ public class Chat {
       senderChannel.queueDeclare(userName, false, false, false, null);
       consumerChannel.basicConsume(userName, true, consumer);
 
-      Commands commandsExecutor = new Commands(connection, userName, currentChatWith);
+      WebTarget webTarget = ClientBuilder
+          .newClient()
+          .target("http://rabbitmq-sd-20222-lb-0c36c1cf9b9311ed.elb.us-east-1.amazonaws.com/api");
+      Commands commandsExecutor = new Commands(
+          connection,
+          userName,
+          currentChatWith,
+          webTarget,
+          "admin:rabbitmq2022.2");
 
       System.out.print("\n>> ");
       String userInput = input.nextLine();
